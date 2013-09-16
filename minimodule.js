aae9da3cf4914699193d2133d3645de9cd128fde
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-09-16 10:39:00 +0200

    added test html

--- minimodule.js
+++ minimodule.js
@@ -1,6 +1,26 @@
 (function(window) {
 	window.mini = window.mini || {};
-	window.mini.Module = function() { return this; };
-	window.mini.requires = function() { return this; };
+	
+	var currentModule = "";
+	
+	// load a single script file
+	window.mini.pathPrefix = "";
+	var loadScript = function() {};
+	window.mini.loadscript = loadScript;
+	
+	var Module = function(name) {
+		this.name = name;
+	};
+
+	window.mini.Module = function(name) {
+		currentModule = name;
+		return this;
+	};
+	
+	window.mini.requires = function() {
+		return this;
+	};
+	
 	window.mini.run = function() {};
+
 })(window);
